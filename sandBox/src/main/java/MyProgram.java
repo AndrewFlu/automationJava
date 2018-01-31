77dc61f4472f69df6d20c5e2d4542ef53534245b
Shalagin Andrey <andreyshalagin@gmail.com>
2018-01-31 18:50:08 +0300

    внесение правок в gitignore

---
+++ sandBox/src/main/java/MyProgram.java
@@ -0,0 +1,8 @@
+public class MyProgram {
+	
+	public static void main (String [] args){
+		System.out.println("Hello, my dear and lovely Andrew!");
+		System.out.println("Hello, my dear!");
+		
+	}
+}
